13d84e2c49ee8682339b2c2406583d4fcce5ecbe
santu8597 <162991657+santu8597@users.noreply.github.com>
2025-04-25 12:40:19 +0530

    readme added

--- client/src/app/api/chat/route.ts
+++ client/src/app/api/chat/route.ts
@@ -3,9 +3,8 @@ import { mastra } from '@/mastra'
  
 export async function POST(req: Request) {
   const { messages } = await req.json()
+  const agent = mastra.getAgent('nextjsAgent')
   
-  const agent = mastra.getAgent('MusicMoodAgent')
- 
   const result = await agent.stream(messages)
  
   return result.toDataStreamResponse()
